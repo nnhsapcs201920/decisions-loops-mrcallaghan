5dd4d1b0377ee59f24738d323eec812515f86ce2
mattcallaghan <mcallaghan@naperville203.org>
2019-10-28 14:47:21 -0500

    finished ch 6 notes

--- Chapter 6 Notes/Notes.java
+++ Chapter 6 Notes/Notes.java
@@ -82,6 +82,29 @@ public class Notes
         System.out.println("done");
     }
     
+    public static void offByOne()
+    {
+        /*
+         * The classic off-by-one error is common with for loops executing 
+         *      one too many or one too few times.
+         *      
+         *      Carefully stop and ask:  should the initial value start at 0 or 1?
+         *                               should the condition be < or <=?
+         *                               
+         *       "Stop and think!  Don't just try at random."
+         */
+        
+       
+        // we want to print five "*"
+        for (int i = 0;
+             i <= 5;  // should be < 5 instead to fix error
+             i++)
+        {
+            System.out.println("*");
+        }
+        System.out.println("done.");
+    }
+    
     public static void doExample()
     {
         /*
@@ -104,5 +127,106 @@ public class Notes
         while( count <= 5 ) ;            // condition
         
         System.out.println("done");
+    }
+    
+    public static int sum()
+    {
+        Scanner s = new Scanner(System.in);
+        
+        int sum = 0;
+        int value;
+        
+        do
+        {
+            System.out.print("enter a positive integer (-1 to quit: ");
+            
+            /*
+             * Sentinel Value
+             *      value (e.g., -1) used to terminate a loop
+             *      it is often entered by a user
+             */
+            value = s.nextInt();
+            
+            // this is a bug! sum is 1 less than it should be
+            sum += value;
+        }
+        while(value != -1);
+        
+        return sum;
+    }
+    
+    public static int sum2()
+    {
+        Scanner s = new Scanner(System.in);
+        
+        int sum = 0;
+        int value;
+        
+        do
+        {
+            System.out.print("enter a positive integer (-1 to quit): ");
+            value = s.nextInt();
+            
+            if(value == -1)
+            {
+                /*
+                 * break: immediately exits the inner-most loop
+                 *      it is in (similar behavior as used in
+                 *      switch statements)
+                 */
+                break;
+            }
+            
+            sum += value;
+        }
+        while(value != -1);
+        
+        return sum;
+    }
+    
+    public static int sum3()
+    {
+        Scanner s = new Scanner(System.in);
+        
+        int sum = 0;
+        int value = 0;
+        
+        do
+        {
+            System.out.print("enter a positive integer (-1 to quit): ");
+            
+            /*
+             * The hasNextInt method of the Scanner class returns
+             *      true if the next token to be read is an integer;
+             *      otherwise, it returns false. It does not consume
+             *      the next token.
+             */
+            if(s.hasNextInt() == false)
+            {
+                System.out.println(s.next() +   // consume next token
+                        " is not an integer; try again.");
+                /*
+                 * continue:
+                 *      1. immediately skips to the end of the
+                 *          inner-most loop it is in
+                 *      2. re-evaluates the loop condition at that
+                 *          point and continues if the condition is
+                 *          true
+                 */        
+                continue;
+            }
+            
+            value = s.nextInt();
+            
+            if(value == -1)
+            {
+                break;
+            }
+            
+            sum += value;
+        }
+        while(value != -1);
+        
+        return sum;
     }
 }
